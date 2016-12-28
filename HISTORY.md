0d445230592c603c37b7e036008b2d2ac83becbb
yurifrl <yurifrl@hotmail.com>
2016-12-28 18:58:35 -0200

    start c and docker sandbox

---
+++ HISTORY.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=MG91ihb0oZ0&t=387s
+https://github.com/jmoyers/http
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  int* primes = new int[ 1000 ];
+  printf("Hello world\n");
+  return 0;
+}
